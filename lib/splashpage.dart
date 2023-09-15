501543e247ecb972fdbfb38ba32f6b48b44b2f6b
farsanajasila <farsanajasila@gmail.com>
2023-09-16 03:53:50 +0530

    commit

--- lib/splashpage.dart
+++ lib/splashpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){  //to run an app
   runApp(MaterialApp(  //default theme of our flutter app
     home:SplashPage(),  //initial page to be launched while running an app
@@ -10,9 +11,23 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     //BuildContext-used to locate widgets on the widget tree and monitor their actions
     return  Scaffold(
-    backgroundColor: Colors.black,
-      body: Center(
-          child:Column(
+    //backgroundColor: Colors.black
+      body: Container(
+        decoration:const  BoxDecoration(
+          //color: Colors.black38
+          // image: DecorationImage(
+          //   fit:BoxFit.cover,
+          //     image: NetworkImage("https://images.unsplash.com/photo-1694501898581-49fd192e65fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60"))
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            colors:[
+        Colors.green,
+        Colors.white70,
+        Colors.black12
+            ])
+        ),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ///adding built in icons
@@ -28,18 +43,24 @@ class SplashPage extends StatelessWidget {
             //  width: 100 ,
             //  height: 100,
             // ),
-            Image(
-                 image: NetworkImage("https://icons.iconarchive.com/icons/elegantthemes/softies/128/Compose-icon.png"),
+            ///network image as image provider
+            const Image(
+                 image: NetworkImage("https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/template_x_application_office-512.png"),
                  width: 100,height: 100,
              ),
             Text(
               "My Application",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.green,
-            fontWeight: FontWeight.bold),
+            style: GoogleFonts.dancingScript(
+              fontSize:30,
+              color:Colors.lightBlueAccent,
+              fontWeight:FontWeight.bold
+            ),
+            //   style: TextStyle(
+            //     fontSize: 20,
+            //     color: Colors.green,
+            // fontWeight: FontWeight.bold),
             )
-          ],
+              ],
           ),
     ),
     );
